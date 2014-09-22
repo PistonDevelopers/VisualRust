8032c515a1e3a67cc9f196a7d3a0d974877d584d
Andrzej Janik <vosen@vosen.pl>
2014-09-22 22:58:07 +0200

    Pass #[path = ...] attributed modules to the project system

--- VisualRust.Project/ModuleTracker.cs
+++ VisualRust.Project/ModuleTracker.cs
@@ -36,35 +36,47 @@ namespace VisualRust.Project
             fileRoots.Add(normalized);
         }
 
-        private static string PathGetRelativeOrFull(string source, string target)
-        {
-            Contract.Assert(source.EndsWith(@"\"));
-            // This is undocumented, but GetFullPath normalizes the path, eg turns c:\foo\..\bar into c:\bar
-            string normalizedTarget = Path.GetFullPath(target);
-            int commonLength = 0;
-            int lastSlash = -1;
-            for (; commonLength < Math.Min(source.Length, normalizedTarget.Length); commonLength++)
-            {
-                if (source[commonLength] != normalizedTarget[commonLength])
-                    break;
-                if (source[commonLength] == Path.DirectorySeparatorChar || source[commonLength] == Path.AltDirectorySeparatorChar)
-                    lastSlash = commonLength;
-            }
-            string pathFromSlash = normalizedTarget.Substring(lastSlash + 1, normalizedTarget.Length - lastSlash - 1);
-            if (lastSlash != -1 && commonLength != source.Length && pathFromSlash.LastIndexOf('\\') != -1)
-                return @"..\" + pathFromSlash;
-            else
-                return pathFromSlash;
-        }
-
         public HashSet<string> ParseReachableNonRootModules()
         {
             HashSet<string> reached = new HashSet<string>();
+            HashSet<string> reachedAuthorative = new HashSet<string>();
             foreach(string root in this.fileRoots)
             {
-                ExtractReachableModules(reached, this.fileRoots, root, ReadImports);
+                ExtractReachableModules(reached, reachedAuthorative, this.fileRoots, root, ReadImports);
             }
-            return reached;
+
+            /*
+             * Reached non-authorative module imports are standard stuff like mod foo { mod bar; }
+             * (as opposed to mod foo { #[path="bar.rs"] bar; }).
+             * Now, for every terminal module import foo/bar we find out actual backing file in this order:
+             * Check for authorative entry foo/bar.rs
+             * Check for authorative entry foo/bar/mod.rs
+             * Check on the disk for foo/bar.rs
+             * Check on the disk for foo/bar/mod.rs
+             * If all of the above fails go with broken foo/bar.rs
+             */
+            foreach(string file in reached)
+            {
+                string filePath = file + ".rs";
+                string subfolderPath = Path.Combine(file, "mod.rs");
+                if (reachedAuthorative.Contains(filePath) || reachedAuthorative.Contains(subfolderPath))
+                {
+                    continue;
+                }
+                else if (File.Exists(filePath))
+                {
+                    reachedAuthorative.Add(filePath);
+                }
+                else if(File.Exists(subfolderPath))
+                {
+                    reachedAuthorative.Add(subfolderPath);
+                }
+                else
+                {
+                    reachedAuthorative.Add(filePath);
+                }
+            }
+            return reachedAuthorative;
         }
 
         private static ModuleImport ReadImports(string path)
@@ -99,7 +111,8 @@ namespace VisualRust.Project
          *       # open the file and recursively call ExtractReachableModules
          *     # else do nothing, we will parse this import when going through roots sooner or later
          */
-        private static void ExtractReachableModules(HashSet<string> reachable,
+        private static void ExtractReachableModules(HashSet<string> reachable, 
+                                                    HashSet<string> reachableAuthorative,
                                                     HashSet<string> roots,
                                                     string importPath,
                                                     Func<string, ModuleImport> importReader) // the last argument is there just to make unit-testing possible
@@ -110,93 +123,62 @@ namespace VisualRust.Project
                 string terminalImportPath = Path.Combine(Path.GetDirectoryName(importPath), Path.Combine(import.Select(i => i.Name).ToArray()));
                 if (!roots.Contains(terminalImportPath) && !reachable.Contains(terminalImportPath))
                 {
-                    reachable.Add(terminalImportPath);
-                    ExtractReachableModules(reachable, roots, terminalImportPath, importReader);
+                    if(import[import.Length - 1].IsAuthorative)
+                        reachableAuthorative.Add(terminalImportPath);
+                    else
+                        reachable.Add(terminalImportPath);
+                    ExtractReachableModules(reachable, reachableAuthorative, roots, terminalImportPath, importReader);
                 }
             }
         }
 
 #if TEST
+        [TestFixture]
         private class Test
         {
-            [TestFixture]
-            private class PathGetRelativeOrFull
+            [Test]
+            public void ExtractSimple()
             {
-                [Test]
-                public void GetPathRelative()
-                {
-                    Assert.AreEqual(@"file.txt", PathGetRelativeOrFull(@"C:\", @"C:\foo\..\bar\..\file.txt"));
-                    Assert.AreEqual(@"src\main.rs", PathGetRelativeOrFull(@"C:\dev\", @"C:\dev\..\dev\src\main.rs"));
-                }
-
-                [Test]
-                public void GetPathFull()
-                {
-                    Assert.AreEqual(@"D:\file.txt", PathGetRelativeOrFull(@"C:\", @"D:\foo\..\bar\..\file.txt"));
-                }
-
-                [Test]
-                public void GetPathRelativeAbove()
-                {
-                    Assert.AreEqual(@"..\baz\file.txt", PathGetRelativeOrFull(@"C:\foo\bar\", @"C:\foo\baz\file.txt"));
-                }
-
-                [Test]
-                public void Normalization()
-                {
-                    var tracker = new ModuleTracker(@"C:\dev\src\main.rs");
-                    tracker.AddRoot(@"C:\dev\..\dev\src\main.rs");
-                    Assert.AreEqual(1, tracker.fileRoots.Count);
-                }
-            }
-
-            [TestFixture]
-            private class ExtractReachableModules
-            {
-                [Test]
-                public void ExtractSimple()
-                {
-                    // src\main.rs referenced nothing, we now scan src\foo.rs, which references two empty modules
-                    var reachable = new HashSet<string>();
-                    var roots = new HashSet<string>() { @"C:\dev\app\src\main.rs", @"C:\dev\app\src\foo.rs" };
-                    var importPath = @"C:\dev\app\src\foo.rs";
-                    var imports = new ModuleImport()
+                // src\main.rs referenced nothing, we now scan src\foo.rs, which references two empty modules
+                var reachable = new HashSet<string>();
+                var roots = new HashSet<string>() { @"C:\dev\app\src\main.rs", @"C:\dev\app\src\foo.rs" };
+                var importPath = @"C:\dev\app\src\foo.rs";
+                var imports = new ModuleImport()
                     {
                         { new PathSegment("bar.rs"), new ModuleImport() },
                         { new PathSegment("baz.rs"), new ModuleImport() }
                     };
-                    ExtractReachableModules(reachable, roots, importPath, (s) => s.EndsWith("foo.rs") ? imports : new ModuleImport());
-                    Assert.AreEqual(2, reachable.Count);
-                    Assert.True(reachable.Contains(@"C:\dev\app\src\bar.rs"));
-                    Assert.True(reachable.Contains(@"C:\dev\app\src\baz.rs"));
-                }
+                ExtractReachableModules(reachable, new HashSet<string>(), roots, importPath, (s) => s.EndsWith("foo.rs") ? imports : new ModuleImport());
+                Assert.AreEqual(2, reachable.Count);
+                Assert.True(reachable.Contains(@"C:\dev\app\src\bar.rs"));
+                Assert.True(reachable.Contains(@"C:\dev\app\src\baz.rs"));
+            }
 
 
-                [Test]
-                public void ExtractNested()
-                {
-                    // src\main.rs referenced nothing, we now scan src\foo.rs, which references three modules
-                    var reachable = new HashSet<string>();
-                    var roots = new HashSet<string>() { @"C:\dev\app\src\main.rs", @"C:\dev\app\src\foo.rs", @"C:\dev\app\src\bar.rs" };
-                    var importPath = @"C:\dev\app\src\foo.rs";
-                    var imports = new ModuleImport()
+            [Test]
+            public void ExtractNested()
+            {
+                // src\main.rs referenced nothing, we now scan src\foo.rs, which references three modules
+                var reachable = new HashSet<string>();
+                var roots = new HashSet<string>() { @"C:\dev\app\src\main.rs", @"C:\dev\app\src\foo.rs", @"C:\dev\app\src\bar.rs" };
+                var importPath = @"C:\dev\app\src\foo.rs";
+                var imports = new ModuleImport()
                     {
                         { new PathSegment("bar.rs"), new ModuleImport() },
                         { new PathSegment("baz.rs"), new ModuleImport() },
                         { new PathSegment("frob.rs"), new ModuleImport() },
                     };
-                    var frobImports = new ModuleImport()
+                var frobImports = new ModuleImport()
                     {
                         { new PathSegment("in1"), new ModuleImport() {
                             { new PathSegment("in2.rs"), new ModuleImport() }
                         }}
                     };
-                    ExtractReachableModules(reachable, roots, importPath, (s) => s.EndsWith("foo.rs") ? imports : s.EndsWith("frob.rs") ? frobImports : new ModuleImport());
-                    Assert.AreEqual(3, reachable.Count);
-                    Assert.True(reachable.Contains(@"C:\dev\app\src\baz.rs"));
-                    Assert.True(reachable.Contains(@"C:\dev\app\src\frob.rs"));
-                    Assert.True(reachable.Contains(@"C:\dev\app\src\in1\in2.rs"));
-                }
+                ExtractReachableModules(reachable, new HashSet<string>(), roots, importPath, (s) => s.EndsWith("foo.rs") ? imports : s.EndsWith("frob.rs") ? frobImports : new ModuleImport());
+                Assert.AreEqual(3, reachable.Count);
+                Assert.True(reachable.Contains(@"C:\dev\app\src\baz.rs"));
+                Assert.True(reachable.Contains(@"C:\dev\app\src\frob.rs"));
+                Assert.True(reachable.Contains(@"C:\dev\app\src\in1\in2.rs"));
             }
         }
 #endif

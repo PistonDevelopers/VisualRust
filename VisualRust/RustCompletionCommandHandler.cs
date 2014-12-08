a577357ae38d3afa2ad1fce6b381b96d7c9c1e15
Anders Forsgren <anders.forsgren@gmail.com>
2014-12-08 15:17:53 +0100

    Racer autocomplete

---
+++ VisualRust/RustCompletionCommandHandler.cs
@@ -0,0 +1,277 @@
+using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Antlr4.Runtime;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VisualRust
+{
+   [Export(typeof(IVsTextViewCreationListener))]
+   [ContentType("rust")]
+   [TextViewRole(PredefinedTextViewRoles.Interactive)]
+   internal sealed class RustCompletionHandlerProvider : IVsTextViewCreationListener
+   {
+      [Import]
+      IVsEditorAdaptersFactoryService AdapterService { get; set; }
+
+      [Import]
+      internal SVsServiceProvider ServiceProvider { get; set; }
+
+      [Import]
+      internal ICompletionBroker CompletionBroker { get; set; }
+
+      public void VsTextViewCreated(IVsTextView textViewAdapter)
+      {
+         IWpfTextView textView = AdapterService.GetWpfTextView(textViewAdapter);
+         if (textView == null)
+            return;
+
+         Func<RustCompletionCommandHandler> createCommandHandler =
+            () => new RustCompletionCommandHandler(textViewAdapter, textView, CompletionBroker);
+         textView.Properties.GetOrCreateSingletonProperty(createCommandHandler);
+      }
+   }
+
+   internal sealed class RustCompletionCommandHandler : IOleCommandTarget
+   {
+      private readonly IOleCommandTarget next;
+      private ICompletionSession currentSession;
+
+      public RustCompletionCommandHandler(IVsTextView viewAdapter, ITextView textView, ICompletionBroker broker)
+      {
+         currentSession = null;
+         TextView = textView;
+         Broker = broker;
+         viewAdapter.AddCommandFilter(this, out next);
+      }
+
+      public ITextView TextView { get; private set; }
+
+      public ICompletionBroker Broker { get; private set; }
+
+      private char GetTypeChar(IntPtr pvaIn)
+      {
+         return (char)(ushort)Marshal.GetObjectForNativeVariant(pvaIn);
+      }
+
+      private bool IsSessionStarted
+      {
+         get { return currentSession != null && currentSession.IsStarted && !currentSession.IsDismissed; }
+      }
+
+      public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+      {
+         bool handled = false;
+         int hresult = VSConstants.S_OK;
+
+         if (pguidCmdGroup == VSConstants.VSStd2K)
+         {
+            switch ((VSConstants.VSStd2KCmdID)nCmdID)
+            {
+               case VSConstants.VSStd2KCmdID.AUTOCOMPLETE:
+               case VSConstants.VSStd2KCmdID.COMPLETEWORD:
+               case VSConstants.VSStd2KCmdID.SHOWMEMBERLIST:
+                  handled = StartSession();
+                  break;
+               case VSConstants.VSStd2KCmdID.RETURN:
+                  handled = Complete(false);
+                  break;
+               case VSConstants.VSStd2KCmdID.TAB:
+                  handled = Complete(true);
+                  break;
+               case VSConstants.VSStd2KCmdID.CANCEL:
+                  handled = Cancel();
+                  break;
+            }
+         }
+
+         if (!handled)
+            hresult = next.Exec(pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+
+         if (ErrorHandler.Succeeded(hresult))
+         {
+            if (pguidCmdGroup == VSConstants.VSStd2K)
+            {
+               switch ((VSConstants.VSStd2KCmdID)nCmdID)
+               {
+                  case VSConstants.VSStd2KCmdID.TYPECHAR:
+                     char ch = GetTypeChar(pvaIn);
+                     var tokensAtCursor = GetTokensAtCursor();
+
+                     var leftTokenType = tokensAtCursor.Item1;
+                     var currentTokenType = tokensAtCursor.Item2;
+
+                     RustTokenTypes[] cancelTokens = { RustTokenTypes.COMMENT, RustTokenTypes.STRING, RustTokenTypes.DOC_COMMENT, RustTokenTypes.WHITESPACE };
+
+                     if (char.IsControl(ch)
+                         || ch == ';'
+                         || (leftTokenType.HasValue && cancelTokens.Contains(leftTokenType.Value))
+                         || (currentTokenType.HasValue && cancelTokens.Contains(currentTokenType.Value)))
+                     {
+                        Cancel();
+                     }
+                     else if (leftTokenType == RustTokenTypes.STRUCTURAL)
+                     {
+                        Cancel();
+                        StartSession();
+                     }
+                     else if (leftTokenType == RustTokenTypes.IDENT && currentTokenType == null)
+                     {
+                        StartSession();
+                     }
+                     else if (IsSessionStarted)
+                     {
+                        string applicableTo = currentSession.SelectedCompletionSet.ApplicableTo.GetText(currentSession.TextView.TextSnapshot);
+                        if (string.IsNullOrEmpty(applicableTo))
+                           RestartSession();
+                        else
+                           Filter();
+                     }
+                     break;
+                  case VSConstants.VSStd2KCmdID.BACKSPACE:
+                  case VSConstants.VSStd2KCmdID.DELETE:
+                  case VSConstants.VSStd2KCmdID.DELETEWORDLEFT:
+                  case VSConstants.VSStd2KCmdID.DELETEWORDRIGHT:
+                     Filter();
+                     break;
+               }
+            }
+         }
+
+         return hresult;
+      }
+
+      private Tuple<RustTokenTypes?, RustTokenTypes?> GetTokensAtCursor()
+      {
+         SnapshotPoint caret = TextView.Caret.Position.BufferPosition;
+         var line = caret.GetContainingLine();
+         var tokens = Utils.LexString(line.GetText()).ToList();
+
+         if (tokens.Count == 0)
+            return Tuple.Create<RustTokenTypes?, RustTokenTypes?>(null, null);
+
+         int col = caret.Position - line.Start.Position;
+
+         IToken leftToken;
+         IToken currentToken = tokens.FirstOrDefault(t => col > t.StartIndex && col <= t.StopIndex);
+
+         if (currentToken != null)
+         {
+            if (currentToken == tokens.First())
+               leftToken = null;
+            else
+               leftToken = tokens[tokens.IndexOf(currentToken) - 1];
+         }
+         else
+         {
+            leftToken = tokens.Last();
+         }
+
+         RustTokenTypes? leftTokenType = leftToken != null ? (RustTokenTypes?)Utils.LexerTokenToRustToken(leftToken.Text, leftToken.Type) : null;
+         RustTokenTypes? currentTokenType = currentToken != null ? (RustTokenTypes?)Utils.LexerTokenToRustToken(currentToken.Text, currentToken.Type) : null;
+
+         return Tuple.Create(leftTokenType, currentTokenType);
+      }
+
+      private void RestartSession()
+      {
+         if (IsSessionStarted)
+         {
+            currentSession.Dismiss();
+         }
+         StartSession();
+      }
+
+      private void Filter()
+      {
+         if (!IsSessionStarted)
+            return;
+
+         currentSession.SelectedCompletionSet.Filter();
+         if (currentSession.SelectedCompletionSet.Completions.Count == 0)
+            Cancel();
+         else
+            currentSession.SelectedCompletionSet.SelectBestMatch();
+      }
+
+      private bool Cancel()
+      {
+         if (currentSession == null)
+            return false;
+
+         currentSession.Dismiss();
+
+         return true;
+      }
+
+      private bool Complete(bool force)
+      {
+         if (currentSession == null)
+            return false;
+
+         if (!currentSession.SelectedCompletionSet.SelectionStatus.IsSelected && !force)
+         {
+            currentSession.Dismiss();
+            return false;
+         }
+         else
+         {
+            currentSession.Commit();
+            return true;
+         }
+      }
+
+      private bool StartSession()
+      {
+         if (currentSession != null)
+         {
+            Filter();
+            return false;
+         }
+
+         SnapshotPoint caret = TextView.Caret.Position.BufferPosition;
+         ITextSnapshot snapshot = caret.Snapshot;
+
+         if (!Broker.IsCompletionActive(TextView))
+         {
+            currentSession = Broker.CreateCompletionSession(TextView, snapshot.CreateTrackingPoint(caret, PointTrackingMode.Positive), true);
+         }
+         else
+         {
+            currentSession = Broker.GetSessions(TextView)[0];
+         }
+
+         currentSession.Dismissed += (sender, args) => currentSession = null;
+
+         currentSession.Start();
+         Filter();
+         return true;
+      }
+
+      public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+      {
+         if (pguidCmdGroup == VSConstants.VSStd2K)
+         {
+            switch ((VSConstants.VSStd2KCmdID)prgCmds[0].cmdID)
+            {
+               case VSConstants.VSStd2KCmdID.AUTOCOMPLETE:
+               case VSConstants.VSStd2KCmdID.COMPLETEWORD:
+                  prgCmds[0].cmdf = (uint)OLECMDF.OLECMDF_ENABLED | (uint)OLECMDF.OLECMDF_SUPPORTED;
+                  return VSConstants.S_OK;
+            }
+         }
+         return next.QueryStatus(pguidCmdGroup, cCmds, prgCmds, pCmdText);
+      }
+
+
+   }
+}
--- VisualRust/Utils.cs
+++ VisualRust/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,48 @@ namespace VisualRust
 
     static class Utils
     {
+
+        // These keywords are from rustc /src/libsyntax/parse/token.rs, module keywords
+        private static readonly HashSet<string> _kws = new HashSet<string> {
+                "as",
+                "box",
+                "break",
+                "const",
+                "continue",
+                "crate",
+                "else",
+                "enum",
+                "extern",
+                "false",
+                "fn",
+                "for",
+                "if",
+                "impl",
+                "in",
+                "let",
+                "loop",
+                "match",
+                "mod",
+                "move",
+                "mut",
+                "proc",
+                "pub",
+                "ref",
+                "return",
+                "self",
+                "static",
+                "struct",
+                "super",
+                "true",
+                "trait",
+                "type",
+                "unsafe",
+                "use",
+                "virtual",
+                "where",
+                "while"
+            };
+
         public static RustTokenTypes LexerTokenToRustToken(string text, int tok)
         {
             // FIXME: this is super slow.
@@ -77,48 +120,7 @@ namespace VisualRust
             _tt[RustLexer.WHITESPACE] = RustTokenTypes.WHITESPACE;
             _tt[RustLexer.DOC_COMMENT] = RustTokenTypes.DOC_COMMENT;
             _tt[RustLexer.COMMENT] = RustTokenTypes.COMMENT;
-
-            // These keywords are from rustc /src/libsyntax/parse/token.rs, module keywords
-            var _kws = new HashSet<string> {
-                "as",
-                "box",
-                "break",
-                "const",
-                "continue",
-                "crate",
-                "else",
-                "enum",
-                "extern",
-                "false",
-                "fn",
-                "for",
-                "if",
-                "impl",
-                "in",
-                "let",
-                "loop",
-                "match",
-                "mod",
-                "move",
-                "mut",
-                "proc",
-                "pub",
-                "ref",
-                "return",
-                "self",
-                "static",
-                "struct",
-                "super",
-                "true",
-                "trait",
-                "type",
-                "unsafe",
-                "use",
-                "virtual",
-                "where",
-                "while"
-            };
-
+            
             RustTokenTypes ty = _tt[tok];
             if (ty == RustTokenTypes.IDENT)
             {
@@ -152,5 +154,42 @@ namespace VisualRust
                 }
             }
         }
+
+        public static IEnumerable<string> Keywords
+        {
+            get { return _kws; }
+        }
     }
+
+    public class TemporaryFile : IDisposable
+    {
+        public string Path { get; private set; }
+
+        /// <summary>
+        ///  Creates a new, empty temporary file
+        /// </summary>
+        public TemporaryFile()
+        {
+            Path = System.IO.Path.GetTempFileName();            
+        }
+
+        /// <summary>
+        ///  Creates a new temporary file with the argument string as UTF-8 content.
+        /// </summary>
+        public TemporaryFile(string content)
+            : this()
+        {
+            using (var sw = new StreamWriter(Path, false, new UTF8Encoding(false)))
+            {
+                sw.Write(content);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!File.Exists(Path))                      
+                File.Delete(Path);            
+        }
+    }
+
 }

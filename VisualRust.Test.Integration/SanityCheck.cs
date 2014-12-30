a114e8192606939f64b37ae92ffa3fcedae07ffc
Andrzej Janik <vosen@vosen.pl>
2014-12-30 22:00:29 +0100

    Add an integration test
    This simple test checks if we explode during loading/unloading of the extension package. It's not much, but better than nothing.

---
+++ VisualRust.Test.Integration/SanityCheck.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VsSDK.UnitTestLibrary;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualRust.Test.Integration.Fake;
+
+namespace VisualRust.Test.Integration
+{
+    public class SanityCheck
+    {
+        [Test]
+        public void SetSite()
+        {
+            IVsPackage package = new VisualRustPackage() as IVsPackage;
+            OleServiceProvider serviceProvider = OleServiceProvider.CreateOleServiceProviderWithBasicServices();
+            serviceProvider.AddService(typeof(SVsActivityLog), new FakeIVsActivityLog(), true);
+            serviceProvider.AddService(typeof(SVsSolution), new FakeIVsSolution(), true);
+            serviceProvider.AddService(typeof(SVsRegisterProjectTypes), new FakeIVsRegisterProjectTypes(), true);
+            serviceProvider.AddService(typeof(SOleComponentManager), new FakeIOleComponentManager(), true);
+            Assert.AreEqual(0, package.SetSite(serviceProvider));
+            Assert.AreEqual(0, package.SetSite(null));
+        }
+
+    }
+}

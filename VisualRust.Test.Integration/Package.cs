f26473c22f4d3b73b7b9fe533d5269cdab996923
Andrzej Janik <vosen@vosen.pl>
2015-01-01 10:40:46 +0100

    Add tests for creation of project templates

--- VisualRust.Test.Integration/Package.cs
+++ VisualRust.Test.Integration/Package.cs
@@ -13,6 +13,8 @@ namespace VisualRust.Test.Integration
     [TestClass]
     public class Package
     {
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         [HostType("VS IDE")]
         [TestProperty("VsHiveName", "12.0Exp")]
@@ -27,6 +29,34 @@ namespace VisualRust.Test.Integration
                 Guid visualRust = new Guid("40c1d2b5-528b-4966-a7b1-1974e3568abe");
                 Assert.AreEqual(VSConstants.S_OK, shellService.LoadPackage(ref visualRust, out pkg));
                 Assert.IsNotNull(pkg);
+            });
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        [TestProperty("VsHiveName", "12.0Exp")]
+        public void CreateRustLibraryProject()
+        {
+            UIThreadInvoker.Invoke((Action)delegate()
+            {
+                Utils.CreateEmptySolution(TestContext.TestDir, Utils.GetCallingFunction());
+                Assert.AreEqual<int>(0, Utils.ProjectCount());
+                Utils.CreateProjectFromTemplate(Utils.GetCallingFunction(), "Rust Library", "Rust", false);
+                Assert.AreEqual<int>(1, Utils.ProjectCount());
+            });
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        [TestProperty("VsHiveName", "12.0Exp")]
+        public void CreateRustApplicationProject()
+        {
+            UIThreadInvoker.Invoke((Action)delegate()
+            {
+                Utils.CreateEmptySolution(TestContext.TestDir, Utils.GetCallingFunction());
+                Assert.AreEqual<int>(0, Utils.ProjectCount());
+                Utils.CreateProjectFromTemplate(Utils.GetCallingFunction(), "Rust Application", "Rust", false);
+                Assert.AreEqual<int>(1, Utils.ProjectCount());
             });
         }
     }

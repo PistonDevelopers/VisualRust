a3dfbc33be787f58629b97171ca58d320c228a85
Andrzej Janik <vosen@vosen.pl>
2015-04-19 02:05:41 +0200

    Add integration tests for msbuild clean

--- VisualRust.Test.Integration/Project.cs
+++ VisualRust.Test.Integration/Project.cs
@@ -6,6 +6,7 @@ using Microsoft.Deployment.WindowsInstaller;
 using System.IO;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Logging;
+using Microsoft.Build.Evaluation;
 
 namespace VisualRust.Test.Integration
 {
@@ -51,11 +52,57 @@ namespace VisualRust.Test.Integration
         [TestMethod]
         public void BuildTrivialProject()
         {
-            var proj = new Microsoft.Build.Evaluation.Project(@"MSBuild\Trivial\trivial.rsproj");
+            var proj = new ProjectCollection().LoadProject(@"MSBuild\Trivial\trivial.rsproj");
             AssertBuildsProject(proj, "Build");
             Assert.IsTrue(File.Exists(@"MSBuild\Trivial\target\Debug\trivial.exe"));
             Directory.Delete(@"MSBuild\Trivial\obj", true);
             Directory.Delete(@"MSBuild\Trivial\target", true);
+        }
+
+        [TestCategory("Explicit")]
+        [TestMethod]
+        public void CleanWithoutBuild()
+        {
+            AssertBuildEnviromentIsClean(@"MSBuild\Trivial");
+            var proj = new ProjectCollection().LoadProject(@"MSBuild\Trivial\trivial.rsproj");
+            AssertBuildsProject(proj, "Clean");
+            AssertBuildEnviromentIsClean(@"MSBuild\Trivial");
+        }
+
+        [TestCategory("Explicit")]
+        [TestMethod]
+        public void CleanAfterBuild()
+        {
+            var proj = new ProjectCollection().LoadProject(@"MSBuild\Trivial\trivial.rsproj");
+            AssertBuildsProject(proj, "Build");
+            AssertBuildsProject(proj, "Clean");
+            AssertBuildEnviromentIsClean(@"MSBuild\Trivial");
+        }
+
+        private static void AssertBuildEnviromentIsClean(string path)
+        {
+            if(Directory.Exists(Path.Combine(path, "obj")))
+                AssertDirectoryIsEmpty(Path.Combine(path, "obj"));
+            if(Directory.Exists(Path.Combine(path, "target")))
+                AssertDirectoryIsEmpty(Path.Combine(path, "target"));
+        }
+
+
+        private static void AssertDirectoryIsEmpty(string path)
+        {
+            var files = Directory.EnumerateFiles(path).ToArray();
+            if(files.Length > 0)
+            {
+                Assert.Fail(
+                    "Directory {0} not empty. Files: {1}",
+                    path,
+                    String.Join(", ", files.Select(f => f.Substring(path.Length + 1))));
+            }
+            else
+            {
+                foreach(var dir in Directory.GetDirectories(path))
+                AssertDirectoryIsEmpty(dir);
+            }
         }
 
         private static void AssertBuildsProject(Microsoft.Build.Evaluation.Project proj, string target)

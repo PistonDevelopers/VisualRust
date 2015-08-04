48de3955d2a876ca0dbfee8d1122d846a297027a
Andrzej Janik <vosen@vosen.pl>
2015-08-04 21:37:24 +0200

    Remove unused references

--- Microsoft.VisualStudio.Project/ProjectReferenceNode.cs
+++ Microsoft.VisualStudio.Project/ProjectReferenceNode.cs
@@ -21,7 +21,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using Microsoft.CSharp.RuntimeBinder;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 

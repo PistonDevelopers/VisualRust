5b9129e136321c96420dad6b1dd369ef8f4b8f27
Patrick Reisert <kpreisert@gmail.com>
2016-10-21 12:30:23 +0200

    Try to temporarily fix CI release build
    ... by removing unused VisualRust.Cargo project from solution
    
    This is supposed to be only a temporary workaround. The debug build can't be fixed
    without changing the AppVeyor configuration, which is unfortunately not checked in as YAML.

--- VisualRust.Test/Cargo/ManifestTests.cs
+++ VisualRust.Test/Cargo/ManifestTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -175,3 +176,4 @@ namespace VisualRust.Test.Cargo
         }
     }
 }
+*/
